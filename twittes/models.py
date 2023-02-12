4ac4f9ff4d6990dcca9ad7c372aa23450dcbd78f
sajjad <faniam321@gmail.com>
2023-02-13 00:01:23 +0330

    add new model

--- twittes/models.py
+++ twittes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Twittes(models.Model):
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, verbose_name=_('User')
+    )
+    message = models.TextField(verbose_name=_('Message'))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.user.username
