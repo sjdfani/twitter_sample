5b1114727f14262aac6f61e2de287493021691e9
sajjad <faniam321@gmail.com>
2023-02-13 00:00:56 +0330

    add new app

--- twitter/settings.py
+++ twitter/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'users.apps.UsersConfig',
+    'twittes.apps.TwittesConfig',
 ]
 
 MIDDLEWARE = [
--- twitter/urls.py
+++ twitter/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/users/', include('users.urls')),
+    path('api/v1/twittes/', include('twittes.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
