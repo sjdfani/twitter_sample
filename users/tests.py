d54d762b76a3377031a394ec583be05c19e34ed9
sajjad <faniam321@gmail.com>
2023-02-13 17:37:05 +0330

    complete test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestSetUp(APITestCase):
+    def setUp(self) -> None:
+        self.sign_up_url = reverse('users:sign_up_user')
+        self.sign_in_url = reverse('users:sign_in_user')
+        self.user_data = {
+            'username': 'test',
+            'password': '123456'
+        }
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+
+class TestViews(TestSetUp):
+
+    def test_user_cannot_sign_up_correctly(self):
+        response = self.client.post(self.sign_up_url)
+        self.assertEqual(response.status_code, 400)
+
+    def test_user_can_sign_up_correctly(self):
+        response = self.client.post(
+            self.sign_up_url, self.user_data, format='json')
+        self.assertEqual(response.data['username'], self.user_data['username'])
+        self.assertEqual(response.status_code, 201)
+
+    def test_user_cannot_sign_in_correctly(self):
+        self.client.post(self.sign_up_url, self.user_data, format='json')
+        user_data = {
+            'username': self.user_data['username'],
+            'password': '12345678',
+        }
+        response = self.client.post(
+            self.sign_in_url, user_data, format='json'
+        )
+        self.assertEqual(response.status_code, 401)
+
+    def test_user_can_sign_in_correctly(self):
+        self.client.post(self.sign_up_url, self.user_data, format='json')
+        response = self.client.post(
+            self.sign_in_url, self.user_data, format='json'
+        )
+        self.assertEqual(response.status_code, 200)
