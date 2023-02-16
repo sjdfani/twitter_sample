765d4c8c93227a7de92712325c9c3050f0133750
sajjad <faniam321@gmail.com>
2023-02-16 23:02:19 +0330

    complete admin

--- twittes/admin.py
+++ twittes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Twittes
 
-# Register your models here.
+
+class TwittesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'updated_at')
+    list_filter = ('user',)
+    search_fields = ('user',)
+
+
+admin.site.register(Twittes, TwittesAdmin)
