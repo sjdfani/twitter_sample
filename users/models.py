d4864050daf9ffdff24ca696893ffbf1a1fc6b88
sajjad <faniam321@gmail.com>
2023-02-16 23:12:31 +0330

    add alter

--- users/models.py
+++ users/models.py
@@ -7,19 +7,21 @@ from .manager import CustomUserManager
 class User(AbstractUser):
     first_name = None
     last_name = None
-    username = models.CharField(_('Username'), max_length=20)
-    email = models.EmailField(_("Email address"), unique=True)
+    username = models.CharField(verbose_name=_('Username'), max_length=20)
+    email = models.EmailField(verbose_name=_("Email address"), unique=True)
     fullname = models.CharField(
-        _('Fullname'), max_length=50, null=True, blank=True)
+        verbose_name=_('Fullname'), max_length=50, null=True, blank=True)
     bio = models.TextField(_('Bio'), null=True, blank=True)
     locations = models.CharField(
-        _('Locations'), max_length=50, null=True, blank=True)
+        verbose_name=_('Locations'), max_length=50, null=True, blank=True)
     website = models.CharField(
-        _('Website'), max_length=100, null=True, blank=True)
-    birthdate = models.DateField(_('Birthdate'), null=True, blank=True)
-    cover_photo = models.ImageField(_('Cover photo'), null=True, blank=True)
+        verbose_name=_('Website'), max_length=100, null=True, blank=True)
+    birthdate = models.DateField(
+        verbose_name=_('Birthdate'), null=True, blank=True)
+    cover_photo = models.ImageField(
+        verbose_name=_('Cover photo'), upload_to='cover/', null=True, blank=True)
     profile_photo = models.ImageField(
-        _('Profile photo'), null=True, blank=True)
+        verbose_name=_('Profile photo'), upload_to='profile/', null=True, blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['username',]
