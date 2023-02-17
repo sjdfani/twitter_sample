4185a1a88536240f86886f6577426f74586d5ee9
sajjad <faniam321@gmail.com>
2023-02-17 14:34:32 +0330

    complete media section

--- twittes/serializer.py
+++ twittes/serializer.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
-from .models import Twittes
+from .models import Twittes, TwittesMedia
 from users.serializer import UserSerializer
+
+
+class MediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TwittesMedia
+        fields = '__all__'
 
 
 class TwittesSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
+    media = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Twittes
         fields = '__all__'
+
+    def get_media(self, instance):
+        media = TwittesMedia.objects.filter(twittes__pk=instance.pk)
+        return MediaSerializer(media, many=True).data
 
 
 class CreateTwittesSerializer(serializers.ModelSerializer):
@@ -17,9 +28,14 @@ class CreateTwittesSerializer(serializers.ModelSerializer):
         exclude = ('user',)
 
     def create(self, validated_data):
-        user = self.context['request'].user
-        obj = Twittes.objects.create(user=user, **validated_data)
-        return obj
+        request = self.context['request']
+        twittes = Twittes.objects.create(user=request.user, **validated_data)
+        for name, file in request.FILES.items():
+            TwittesMedia.objects.create(
+                twittes=twittes,
+                media=file,
+            )
+        return twittes
 
 
 class RetrieveUpdateDestroyTwittesSerializer(serializers.ModelSerializer):
